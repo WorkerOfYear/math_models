5a7f2a960680e71a89eec98e53984627842a825a
zeromanwww <zeromanwww@mail.ru>
2023-12-19 18:01:33 +0600

    first commit

---
+++ SolveEquations.py
@@ -0,0 +1,241 @@
+import math
+from typing import Union
+
+
+class CalcOneEqual:
+    def __init__(self) -> None:
+        self.N: int = int(input("Enter count of uknowns: "))
+        self.B: list[list[int]] = self.MakeMatrix()
+        self.c: int = int(input("Enter c: "))
+
+    def MakeMatrix(self) -> list[list[int]]:
+        B = [
+            [1 if i == j + 1 else 0 for j in range(self.N)] for i in range(self.N + 1)]
+        for i in range(self.N):
+            B[0][i] = int(input(f"Enter a{i}: "))
+        return B
+
+    def SearchMin(self) -> Union[bool, tuple[int]]:
+        B = self.B
+        N = self.N
+        d_index: int = 0
+        d: int = 0
+        if N == 1:
+            d = B[0][0]
+
+        for index, item in enumerate(B[0][:]):
+            if item != 0:
+                if abs(item) < abs(B[0][d_index]):
+                    d_index = index
+                    d = item
+                else:
+                    d = B[0][d_index]
+
+        if d == 0:
+            return False
+        else:
+            return d, d_index
+
+    def CalcRQ(self, a: int, b: int):
+        r = a % b
+        q = a // b
+
+        if r < 0 and a * b < 0:
+            q -= 1
+            r = a - b * q
+        if r < 0 and a * b > 0:
+            q += 1
+            r = a - b * q
+
+        return r, q
+
+    def CalcMatrix(self) -> None:
+        r = q = count_not_null = 0
+        N = self.N
+        B = self.B
+
+        while count_not_null != 1:
+
+            res_searchmin = self.SearchMin()
+            if not res_searchmin:
+                raise ValueError("All elements are 0")
+            else:
+                d, d_index = res_searchmin
+
+            self.d = d
+            self.d_index = d_index
+            count_not_null = 1
+
+            for j in range(N):
+                if B[0][j] != 0 and j != d_index:
+                    r, q = self.CalcRQ(B[0][j], d)
+                    B[0][j] = r
+
+                    for i in range(1, N + 1):
+                        B[i][j] -= q * B[i][d_index]
+
+                if r != 0:
+                    count_not_null += 1
+
+    def WriteResult(self) -> None:
+        if self.c % self.d == 0:
+            for i in range(1, self.N + 1):
+                t = 0
+                print(f"\nx{(i-1)} = {self.B[i][self.d_index] * self.c / self.d}", end=" ")
+                for j in range(self.N):
+                    if j != self.d_index:
+                        print(" + " + str(self.B[i][j]) + "*t" + str(t), end="")
+                        t += 1
+        else:
+            raise ValueError("No solutions in integers")
+
+
+class CalcSystemEquals:
+    def __init__(self, m=None, n=None, B=None, write_res=True):
+        if m == None or n == None or B == None:
+            self.m = int(input("Enter count of equals: "))
+            self.n = int(input("Enter count of uknowns: "))
+            self.B: list[list[int]] = self.MakeMatrix()
+            self.write_res: bool = write_res
+            return
+
+        self.write_res: bool = write_res
+        self.m: int = m
+        self.n: int = n
+        self.B: list[list[int]] = B
+
+    def MakeMatrix(self):
+        B = [[1 if (self.m + j) == i else 0 for j in range(self.n + 1)] for i in range(self.m + self.n)]
+        for i in range(self.m):
+            for j in range(self.n):
+                B[i][j] = int(input(f"Enter a{i}{j}: "))
+            B[i][self.n] = -1 * int(input(f"Enter c{i}: "))
+        return B
+    
+    def SearchMin(self, index):
+        _flag = False
+        not_null_index = index
+
+        try:
+            for i in range(not_null_index, self.n):
+                if self.B[index][not_null_index] != 0:
+                    _flag = True
+                    break
+        except IndexError:
+            raise Exception("Матрица введена неверно!")
+
+        # Если в строке одни нули от элемента index, то возвращаем False
+        if not _flag:
+            return False
+        
+        d_index = not_null_index
+        for i in range(not_null_index + 1, self.n):
+            if self.B[index][i] != 0:
+                if abs(self.B[index][i] < abs(self.B[index][d_index])):
+                    d_index = i
+
+        d = self.B[index][d_index]
+
+        return d, d_index
+        
+    def CalcRQ(self, a: int, b: int):
+        r = a % b
+        q = a // b
+
+        if r < 0 and a * b < 0:
+            q -= 1
+            r = a - b * q
+        if r < 0 and a * b > 0:
+            q += 1
+            r = a - b * q
+
+        return r, q
+    
+    def ReplaceRow(self, i_from, i_to):
+        row = []
+        for i in range(self.n + self.m):
+            row.append(self.B[i][i_from])
+            self.B[i][i_from] = self.B[i][i_to]
+            self.B[i][i_to] = row[i]
+
+    
+    def CalcString(self, index):
+        r = q = count_not_null = 0
+
+        while count_not_null != 1:
+
+            res_searchmin = self.SearchMin(index)
+            if not res_searchmin:
+                break
+            
+            d, d_index = res_searchmin
+            self.d = d
+            self.d_index = d_index
+            count_not_null = 1
+
+            for j in range(index, self.n):
+                if self.B[index][j] != 0 and j != d_index:
+                    r, q = self.CalcRQ(self.B[index][j], d)
+                    self.B[index][j] = r
+
+                    for i in range(index + 1, self.n + self.m):
+                        self.B[i][j] -= q * self.B[i][d_index]
+
+                if r != 0:
+                    count_not_null += 1
+
+            if d_index != index:
+                self.ReplaceRow(d_index, index)
+
+    def MakeLastRow(self, index):
+        if self.B[index][index] == 0 and self.B[index][self.n] != 0:
+            return False
+        if self.B[index][index] != 0:
+            if self.B[index][self.n] % self.B[index][index] != 0:
+                return False
+        if self.B[index][index] == 0 and self.B[index][self.n] == 0:
+            return True
+        r, q = self.CalcRQ(self.B[index][self.n], self.B[index][index])
+        self.B[index][self.n] = r
+
+        for i in range(index + 1, self.n + self.m):
+            self.B[i][self.n] -= q * self.B[i][index]
+        return True
+    
+    def CalcEquation(self):
+        flag = True
+        for i in range(self.m):
+            self.CalcString(i)
+            flag = self.MakeLastRow(i)
+            if not flag:
+                break
+        
+        if not flag:
+            raise ValueError("No solutions in integers")
+        
+        if self.write_res:
+            self.WriteResult()
+
+    
+    def WriteResult(self):
+        index = 0
+        while self.B[index][index] != 0 and index < self.m:
+            index += 1
+        
+        for i in range(self.m, self.m + self.n):
+            t = 0
+            print(f"\nx{(i-self.m)} = {self.B[i][self.n]}", end=" ")
+            for j in range(index, self.n):
+                if self.B[i][j] != 0:
+                    print(" + " + str(self.B[i][j]) + "*t" + str(t), end="")
+                    t += 1
+
+
+
+if __name__ == '__main__':
+    # obj: CalcOneEqual = CalcOneEqual()
+    # obj.CalcMatrix()
+    # obj.WriteResult()
+
+    obj = CalcSystemEquals()
+    obj.CalcEquation()
---
+++ tests.py
@@ -0,0 +1,92 @@
+import unittest
+import re
+from unittest import TestCase
+
+from SolveEquations import CalcSystemEquals
+
+class TestCalcSystemEqualsWithSolve(TestCase):
+    def setUp(self) -> None:
+        m = 2
+        n = 3
+        self.B = [[3, 4, 0, -8],
+             [7, 0, 5, -6],
+             [1, 0, 0, 0],
+             [0, 1, 0, 0],
+             [0, 0, 1, 0]]
+        
+        self.CSE_obj = CalcSystemEquals(m, n, self.B, write_res=False)
+
+    def test_return_searchmin(self):
+        d, d_index = self.CSE_obj.SearchMin(index=0)
+        self.assertEquals((d, d_index), (3, 0))
+    
+    def test_calcstring(self):
+        self.CSE_obj.CalcString(index=0)
+        expected_B = [[1, 0, 0, -8], 
+                      [-7, 28, 5, -6], 
+                      [-1, 4, 0, 0], 
+                      [1, -3, 0, 0], 
+                      [0, 0, 1, 0]]
+        
+        self.assertEquals(self.B, expected_B)
+    
+    def test_calcequation(self):
+        self.CSE_obj.CalcEquation()
+        expected_B = [[1, 0, 0, 0], 
+                      [-7, 1, 0, 0], 
+                      [-1, 8, -20, 488], 
+                      [1, -6, 15, -364], 
+                      [0, -11, 28, -682]]
+        
+        self.assertEquals(self.B, expected_B)
+
+
+class TestCalcSystemEqualsWithoutSolve(TestCase):
+    def setUp(self) -> None:
+        m = 2
+        n = 3
+        self.B = [[3, 6, 0, -8],
+             [7, 0, 5, -6],
+             [1, 0, 0, 0],
+             [0, 1, 0, 0],
+             [0, 0, 1, 0]]
+        
+        self.CSE_obj = CalcSystemEquals(m, n, self.B, write_res=False)
+    
+    def test_calcequation(self):
+        self.assertRaises(ValueError, self.CSE_obj.CalcEquation)
+
+
+
+class TestCalcSystemEqualsWith4x4martix(TestCase):
+    def setUp(self) -> None:
+        m = 2
+        n = 3
+        self.B = [
+             [3, 6, 0, 3, -8],
+             [7, 0, 5, 4, -6],
+             [4, 3, 4, 2, -6],
+             [1, 0, 0, 0, 0],
+             [0, 1, 0, 0, 0],
+             [0, 0, 1, 0, 0],
+             [0, 0, 0, 1, 0]]
+        
+        self.CSE_obj = CalcSystemEquals(m, n, self.B)
+    
+
+
+class TestCalcSystemEqualsWith0x0martix(TestCase):
+    def setUp(self) -> None:
+        m = 2
+        n = 3
+        self.B = [[]]
+        
+        self.CSE_obj = CalcSystemEquals(m, n, self.B)
+
+    def test_calcequation(self):
+        self.assertRaises(Exception, self.CSE_obj.CalcEquation)
+
+
+
+if __name__ == "__main__":
+    unittest.main()
